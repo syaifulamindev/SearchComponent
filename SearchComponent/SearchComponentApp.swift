fda5265ba5fb3ac6d15466f89d84615cc9556856
Syaiful Amin <sayfullamin@gmail.com>
2024-04-29 05:14:32 +0700

    initialize project

---
+++ SearchComponent/SearchComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchComponentApp.swift
+//  SearchComponent
+//
+//  Created by saminos on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
