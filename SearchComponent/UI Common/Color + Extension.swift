6f4542c44001bb57f2c57b1344907592c26004b6
Syaiful Amin <sayfullamin@gmail.com>
2024-04-29 11:13:27 +0700

    add color extension

---
+++ SearchComponent/UI Common/Color + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color + Extension.swift
+//  SearchComponent
+//
+//  Created by saminos on 29/04/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
