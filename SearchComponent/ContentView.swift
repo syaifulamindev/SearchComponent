1709dbef4b62b222ca8fa82fd3f0d2805276575f
Syaiful Amin <sayfullamin@gmail.com>
2024-04-29 11:06:38 +0700

    implement SearchView in ContentView

--- SearchComponent/ContentView.swift
+++ SearchComponent/ContentView.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            SearchView(textFieldInput: .constant("tes"))
+                .padding(8)
         }
-        .padding()
+        .background(.gray)
     }
 }
 
